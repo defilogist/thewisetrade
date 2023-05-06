8c1133eca62031c6d4655c48627295785abd1e30
Defilogist <defilogist@thewise.trade>
2023-05-06 02:40:24 +0200

    Add info to the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,2 +1,11 @@
 <template>
+<div class="mt-16 italic text-center">
+If you enjoy what you read, please follow me on
+<a class="bold" href="https://twiter.com/defilogist">Twitter</a> and like my tweets!
+</div>
 </template>
+<style>
+a {
+  font-weight: bold;
+}
+</style>
